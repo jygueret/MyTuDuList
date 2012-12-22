716415c264fb04ac8495d050ca4db11356bddfb0
jy <jy@jy-pc>
2012-12-22 12:23:53 +0100

    test

--- MyTuDuList-common/src/test/java/lu/pragmaconsult/mytudulist/AppTest.java
+++ MyTuDuList-common/src/test/java/lu/pragmaconsult/mytudulist/AppTest.java
@@ -1,39 +1,23 @@
 package lu.pragmaconsult.mytudulist;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+{
+
 
     /**
      * Rigourous Test :-)
      */
-    @org.junit.Test
+    @Test
     public void testApp()
     {
-        assertTrue( true );
+       Assert.assertTrue(true);
     }
 }
